db92e189159e97045ef750e1f6c70e855bbf6e2c
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-11 16:20:55 -0400

    Video class cleanup and commenting.

--- modules/video.py
+++ modules/video.py
@@ -5,14 +5,6 @@ class Video():
         self.rating = rating
         self.copies_available = int(copies_available)
 
-    def get_title(self):
-        return self.title
-    def get_copies_available(self):
-        return self.copies_available
-
-    def increment_copies_available(self, increment):
-        self.copies_available += increment
-
     def __str__(self):
         return (f"""
         ---
@@ -20,4 +12,14 @@ class Video():
         Rating: {self.rating}
         Copies Available: {self.copies_available}
         ---""")
+        
+    def get_title(self):
+        return self.title
+    def get_copies_available(self):
+        return self.copies_available
+
+    # Used to increment or decrement (with negative int) the copies available of a given video.
+    def increment_copies_available(self, increment):
+        self.copies_available += increment
+
 
